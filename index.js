bbaf2093863ffd352f2f920750cffdc4a6594baa
wo-n <wo-n@wo-n.local>
2019-01-12 02:46:32 +0900

    UA情報追加

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const video = document.getElementById('video');
 const button = document.getElementById('button');
+let userAgent = document.getElementById('ua');
 
-
+userAgent.innerText = window.navigator.userAgent;
 b.addEventListener('click', () => {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
   .then(stream => v.srcObject = stream)
